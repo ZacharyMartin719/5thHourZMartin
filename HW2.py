033d91e2cc16e7e6563e1e871b05f44488bd2ebd
Zachary Martin <zm125045@student.wpsok.org>
2024-08-21 13:17:10 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,16 @@
+#Name: Zachary Martin
+#Class: 5Th Hour
+#Assignment: HW2
+
+print("Hello World!")
+
+name = input("")
+
+print("Hello" + name + "!")
+
+x = input("Give me a number")
+y = input("Give me another number")
+
+sum = int(x) + int(y)
+
+print("The sum of these two numbers" ,sum)
