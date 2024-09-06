80a69b0c1db4c10bfc919a4a4d07b9a88e614009
Zachary Martin <zm125045@student.wpsok.org>
2024-09-06 13:07:00 -0500

    Added Updated Playground

--- Pl;ayground.py
+++
@@ -1,9 +0,0 @@
-#Name: Zachary Martin
-#Class: 5Th Hour
-#Assignment: Playground
-
-print("Hello World!")
-
-name=input()
-
-print(input("How are you doing today " + name +"."))
---
+++ Playground.py
@@ -0,0 +1,24 @@
+#Name: Zachary Martin
+#Class: 5TH Hour
+#Assignment: Playground
+
+print("Hello World!")
+
+print("What is your name")
+
+name=input()
+
+print("Nice to meet you " + name + ".")
+
+print(input("How are you doing today " + name +"."))
+
+print("Great!")
+
+list1 = [1852,2345,5876,7908]
+
+list2 = [2890,1400,3456,8926]
+
+sum = list1[1] + list2[3]
+
+print(sum)
+
