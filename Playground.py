188c27f1a676bdf118d0f41c760eeca5e13dd495
Zachary Martin <zm125045@student.wpsok.org>
2024-09-09 13:15:15 -0500

    Added Updated Playground

--- Playground.py
+++ Playground.py
@@ -15,10 +15,8 @@ print(input("How are you doing today " + name +"."))
 print("Great!")
 
 list1 = [1852,2345,5876,7908]
-
 list2 = [2890,1400,3456,8926]
 
 sum = list1[1] + list2[3]
-
 print(sum)
 
