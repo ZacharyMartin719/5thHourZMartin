39161dee7c23e350ae7873b9c33b5e948dd16a4a
Zachary Martin <zm125045@student.wpsok.org>
2025-03-04 12:46:42 -0600

    Added HW21

--- HW21.py
+++ HW21.py
@@ -2,7 +2,6 @@
 #Class: 5th Hour
 #Assignment: HW21
 
-
 #1. Make a nested dictionary with three entries called "sport1", "sport2", and "sport3" containing
 #the name a sport the school partakes in, the amount of players a typical team uses during gameplay
 #(ex: Basketball has 5 players), and if the sport uses a ball or not (as a boolean).
@@ -23,6 +22,7 @@ sports = {
     "Ball": True,
       },
 }
+
 #2. Create a def function that pulls the values from the dictionary as arguments, adds together the
 #players of all three sports, and prints the sum
 def sport_function(a,b,c):
